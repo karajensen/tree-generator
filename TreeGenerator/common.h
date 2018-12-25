21b5f2ea14a404dd0a7d17a175b853493966d659
Kara Jensen <mail@karajensen.com>
2018-12-25 13:21:39 +0100

    Upgrading to vs 2017

--- TreeGenerator/common.h
+++ TreeGenerator/common.h
@@ -16,25 +16,25 @@
 //required for maya includes
 #include <iostream>
 
-#include <maya/MPxCommand.h>
-#include <maya/MFloatArray.h>
-#include <maya/MIntArray.h>
-#include <maya/MGlobal.h>
-#include <maya/MArgDatabase.h>
-#include <maya/MDagModifier.h>
-#include <maya/MArgList.h>
-#include <maya/MStatus.h>
-#include <maya/MObject.h>
-#include <maya/MSyntax.h>
-#include <maya/MProgressWindow.h>
-#include <maya/MFnMesh.h>
-#include <maya/MDagPath.h>
-#include <maya/MFloatPointArray.h>
-#include <maya/MPointArray.h>
-#include <maya/MFnTransform.h>
-#include <maya/MFnNurbsCurve.h>
-#include <maya/MFnDependencyNode.h>
-#include <maya/MSelectionList.h>
+#include "maya/MPxCommand.h"
+#include "maya/MFloatArray.h"
+#include "maya/MIntArray.h"
+#include "maya/MGlobal.h"
+#include "maya/MArgDatabase.h"
+#include "maya/MDagModifier.h"
+#include "maya/MArgList.h"
+#include "maya/MStatus.h"
+#include "maya/MObject.h"
+#include "maya/MSyntax.h"
+#include "maya/MProgressWindow.h"
+#include "maya/MFnMesh.h"
+#include "maya/MDagPath.h"
+#include "maya/MFloatPointArray.h"
+#include "maya/MPointArray.h"
+#include "maya/MFnTransform.h"
+#include "maya/MFnNurbsCurve.h"
+#include "maya/MFnDependencyNode.h"
+#include "maya/MSelectionList.h"
 
 //required for maya includes
 using namespace std;
